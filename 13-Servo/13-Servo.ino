0254f509e95a2e958c1e5503d2f08d66e6bbbdba
Jorge VC <jorgevc@hotmail.com>
2019-04-10 16:29:37 -0600

    Nuevos programas

---
+++ 13-Servo/13-Servo.ino
@@ -0,0 +1,27 @@
+/** @file    13-Servo.ino
+ *  @author  Jorge Villalobos Carvajal (jorgemvc@gmail.com)
+ *  @date    10.Abr.2019  
+ *  @version 1.0 
+ *  
+ *  @brief   Uso de servo motor
+ *
+ *  @licence  MIT License
+ */
+#include <Servo.h>
+
+Servo myservo;   // Instancia de un servo
+int 
+  potPin   = A0,   // Puerto analógico del potenciómetro
+  servoPin = 11, // Puerto PWM para manejo del Servo
+  val;           // Lectura del potenciometro [0-1023]
+
+void setup() {
+  myservo.attach(servoPin);  // Crea la instancia y asigna el puerto
+}  // void setup
+
+void loop() {
+  val = analogRead(potPin);            // Lee el valor del potenciómetro
+  val = map(val, 0, 1023, 0, 180);     // Traduce la lectura a los valores del servo [0-180]
+  myservo.write(val);                  // Establece la posición
+  delay(15);                           // Espera para que el servo se posicione
+}  // void loop
